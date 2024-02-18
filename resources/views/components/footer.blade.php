a657247fbe7ba963978524d08bc32a9a4808fc95
Naoufal <naoufal.labrihmi@gmail.com>
2024-02-18 19:55:43 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,35 +2,32 @@
     <div class="flex flex-col items-center mt-16">
         <div class="flex mb-3 space-x-4">
             <x-socialIcon>
-                <x-slot name="href">https://www.facebook.com/arsitekdibali</x-slot>
+                <x-slot name="href">https://www.facebook.com/</x-slot>
                 <x-slot name="svg">
-                    <x-socialIcons.facebook
-                        class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
+                    <x-socialIcons.facebook class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
                 </x-slot>
             </x-socialIcon>
             <x-socialIcon>
-                <x-slot name="href">https://github.com/bagussuandana</x-slot>
+                <x-slot name="href">https://github.com/NaoufalLabrihmi</x-slot>
                 <x-slot name="svg">
-                    <x-socialIcons.github
-                        class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
+                    <x-socialIcons.github class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
                 </x-slot>
             </x-socialIcon>
             <x-socialIcon>
-                <x-slot name="href">mailto:info@visiarch.com</x-slot>
+                <x-slot name="href">mailto:naoufal.labrihmi@gmail.com</x-slot>
                 <x-slot name="svg">
                     <x-socialIcons.mail class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
                 </x-slot>
             </x-socialIcon>
             <x-socialIcon>
-                <x-slot name="href">https://www.youtube.com/channel/UCTUkNGOjzxVr39Mp-eOMrsg</x-slot>
-                <x-slot name="svg">
-                    <x-socialIcons.youtube
-                        class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
-                </x-slot>
+                <x-slot name="href">https://www.youtube.com//x-slot>
+                    <x-slot name="svg">
+                        <x-socialIcons.youtube class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
+                    </x-slot>
             </x-socialIcon>
         </div>
         <div class="flex space-x-4">
-            <a href="{{route('blog.index')}}">VISIARCH</a>
+            <a href="{{route('blog.index')}}">World</a>
             <div> • </div>
             <div>{{date("Y")}}</div>
             <div> • </div>
