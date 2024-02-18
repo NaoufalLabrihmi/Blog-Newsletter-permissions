85c7c9005570a8e4449666b0a318e72dd0e106a8
Naoufal <naoufal.labrihmi@gmail.com>
2024-02-18 22:31:22 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -14,20 +14,20 @@
                 </x-slot>
             </x-socialIcon>
             <x-socialIcon>
-                <x-slot name="href">mailto:naoufal.labrihmi@gmail.com</x-slot>
+                <x-slot name="href">mailto:</x-slot>
                 <x-slot name="svg">
                     <x-socialIcons.mail class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
                 </x-slot>
             </x-socialIcon>
             <x-socialIcon>
-                <x-slot name="href">https://www.youtube.com//x-slot>
-                    <x-slot name="svg">
-                        <x-socialIcons.youtube class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
-                    </x-slot>
+                <x-slot name="href">https://www.youtube.com/</x-slot>
+                <x-slot name="svg">
+                    <x-socialIcons.youtube class="lg:h-8 dark:text-gray-100 block w-auto h-6 text-gray-600 fill-current" />
+                </x-slot>
             </x-socialIcon>
         </div>
         <div class="flex space-x-4">
-            <a href="{{route('blog.index')}}">World</a>
+            <a href="{{route('blog.index')}}">WORLD</a>
             <div> • </div>
             <div>{{date("Y")}}</div>
             <div> • </div>
